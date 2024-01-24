15e755d09a6a988c75732f5a51ecd74c411de93c
Iván Sánchez <sanchez.diaz.ivan@gmail.com>
2024-01-24 13:55:29 +0100

    feat: created balance-list component

---
+++ src/app/infrastructure/Balance/components/balance-list/balance-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+import { Balance } from '../../../../domain/Balance';
+import { CommonModule } from '@angular/common';
+import { FullnamePipe } from '../../../Common/pipes/fullname.pipe';
+import { AmountPipe } from '../../../Common/pipes/amount.pipe';
+
+@Component({
+  selector: 'app-balance-list',
+  standalone: true,
+  imports: [CommonModule, AmountPipe, FullnamePipe],
+  templateUrl: './balance-list.component.html',
+  styleUrl: './balance-list.component.css'
+})
+export class BalanceListComponent {
+  @Input() balances!: Balance[]
+
+}
